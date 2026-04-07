05ae0e96b44b3ab35f8abaa6784eee3452b1d8ee
Nikko <atuan.nikko@gmail.com>
2026-04-07 14:03:01 +0800

    feat: basic core component and styles

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.section}>
+        <Text>Another piece of text</Text>
+        <Text>And another one</Text>
+      </View>
+      <Button title="Click me" onPress={() => alert('Button clicked!')} />
     </View>
   );
 }
@@ -17,4 +19,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  section: {
+    marginBottom: 10, 
+    alignItems: 'center', 
+    borderWidth: 1, 
+    borderColor: 'blue', 
+    padding: 10 
+  }
 });
