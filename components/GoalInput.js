be0fbaa7dc123d8e8e3bf24a0e49fb79c0ce0d3b
Nikko <atuan.nikko@gmail.com>
2026-05-13 22:22:58 +0800

    feat: added modal overlay

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -21,9 +21,14 @@ export const GoalInput = (props) => {
                     placeholder='Your course goal!' 
                     value={enteredGoalText}
                     onChangeText={goalInputHandler} />
-                <Button 
-                    title='Add Goal' 
-                    onPress={addGoalHandler} />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title='Add Goal' onPress={addGoalHandler} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title='Cancel' />
+                    </View>
+                </View>
             </View>
         </Modal>
     )
@@ -32,17 +37,26 @@ export const GoalInput = (props) => {
 const styles = StyleSheet.create({
     inputContainer: {
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flexDirection: 'column',
+        justifyContent: 'center',
         alignItems: 'center',
+        padding: 16,
         marginBottom: 24,
         borderBottomWidth: 1,
         borderBottomColor: '#cccccc',
     },
+    buttonContainer: {
+        marginTop: 16,
+        flexDirection: 'row'
+    },
+    button: {
+        width: 100,
+        marginHorizontal: 6
+    },
     textInput: {
         borderWidth: 1,
         borderColor: '#cccccc',
-        width: '80%',
+        width: '100%',
         marginRight: 8,
         padding: 8,
     },
